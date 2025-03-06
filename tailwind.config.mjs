69963e5dfd3fd1bf25f147cefdd3cc49a6e14bc2
yuqi.ong <yuqi.ong@atoz-software.tech>
2025-03-06 15:07:08 +0800

    feat(dev): add light and dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ function half(value) {
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		fontFamily: {
 			sans: ['Roboto', 'sans-serif'],
